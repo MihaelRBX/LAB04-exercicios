4fc67bd847b64c3554dd19c88e1c778e69ed4b02
MihaelRBX <145815987+MihaelRBX@users.noreply.github.com>
2024-03-19 18:29:21 -0300

    Add files via upload

---
+++ LAB_04-ex6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+float somatoria(int n){
+    
+    // Não consegui realizar a lógica da funcão de somatória
+
+}
+
+int main() {
+    int n;
+    printf("Digite o valor de N: ");
+    scanf("%d", &n);
+
+    float soma = somatoria(n);
+    printf("O resultado da somatória é aproximadamente: %13.f\n", soma);
+
+    return 0;
+}
